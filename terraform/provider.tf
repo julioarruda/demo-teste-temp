50496acfd7d3ac6e422f44f46edd44087a8d3f50
Julio Arruda <julio.arruda@outlook.com>
2023-07-11 19:50:36 +0000

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,6 +10,10 @@ terraform {
     resource_group_name  = "__state_rg__"
     storage_account_name = "__state_stg_account__"
     container_name       = "__state_container_name__"
+    client_id            = "__state_client_id__"
+    client_secret        = "__state_client_secret__"
+    tenant_id            = "__state_tenant_id__"
+    subscription_id      = "__state_subscription_id__"
     key                  = "sampleapp.tfstate"
   }
 }
